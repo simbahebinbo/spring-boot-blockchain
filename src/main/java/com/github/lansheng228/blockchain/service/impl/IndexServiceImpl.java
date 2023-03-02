5d0ec4c6a315c9a6b8255332af724726598c7d3f
penghuaping <lansheng228@163.com>
2023-03-02 15:32:17 +0800

    整理代码

--- src/main/java/com/github/lansheng228/blockchain/service/impl/IndexServiceImpl.java
+++ src/main/java/com/github/lansheng228/blockchain/service/impl/IndexServiceImpl.java
@@ -2,6 +2,7 @@ package com.github.lansheng228.blockchain.service.impl;
 
 import com.github.lansheng228.blockchain.block.Block;
 import com.github.lansheng228.blockchain.block.Blockchain;
+import com.github.lansheng228.blockchain.response.BlockRet;
 import com.github.lansheng228.blockchain.response.BlocksRet;
 import com.github.lansheng228.blockchain.service.IndexService;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,8 @@ public class IndexServiceImpl implements IndexService {
     @Override
     public BlocksRet chain() {
         int length = 0;
+        List<BlockRet> blockRets = new ArrayList<>();
+
         try {
             Blockchain blockChain = Blockchain.initBlockchainFromDB();
             List<Block> blocks = new ArrayList<>();
@@ -25,6 +28,56 @@ public class IndexServiceImpl implements IndexService {
                 blocks.add(block);
             }
             length = blockChain.getAllBlockHash().size();
+            blocks.forEach(b -> {
+                BlockRet br = new BlockRet();
+                br.setVersion(br.getVersion());
+                br.setHash(b.getHash());
+
+
+//                /**
+//                 * 前一个区块的hash值
+//                 */
+//                @JsonProperty("prev-block-hash")
+//                private String prevBlockHash;
+//
+//                /**
+//                 * 交易信息
+//                 */
+//                @JsonProperty("transactions")
+//                private List<TransactionRet> transactions;
+//
+//                /**
+//                 * 区块创建时间(单位:毫秒)
+//                 */
+//                @JsonProperty("timestamp")
+//                private String timestamp;
+//
+//                /**
+//                 * 工作量证明计数器 挖到时的运算值
+//                 */
+//                @JsonProperty("nonce")
+//                private String nonce;
+//
+//                /**
+//                 * 区块高度
+//                 */
+//                @JsonProperty("height")
+//                private String height;
+//
+//                /**
+//                 * 默克尔树rootHash
+//                 */
+//                @JsonProperty("merkle-root")
+//                private String merkleRoot;
+//
+//                /**
+//                 * 难度目标值
+//                 */
+//                @JsonProperty("target")
+//                private String target;
+
+                blockRets.add(br);
+            });
             log.info("blocks: {}, length: {}", blocks, length);
         } catch (Exception e) {
             log.warn(e.getMessage());
@@ -32,7 +85,7 @@ public class IndexServiceImpl implements IndexService {
 
 
         BlocksRet ret = BlocksRet.builder()
-//                .shortName(cache.getShortName())
+                .blocks(blockRets)
                 .length(Integer.toString(length))
                 .build();
         return ret;
